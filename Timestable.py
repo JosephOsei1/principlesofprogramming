8923b10ba4ab2da8f7c8b36f54b671c01b914b1a
Joseph Osei <122857648+JosephOsei1@users.noreply.github.com>
2025-03-01 18:11:30 +0000

    update

--- Timestable.py
+++ Timestable.py
@@ -2,6 +2,6 @@
 num = int(input("Enter a number: "))   #2. What do you think this line is going to do?
 
 
-print(f"\nTimes Table for {num}:\n     #3. There is an error on this line can you find it and fix it?     
+print(f"\nTimes Table for {num}:\n")     #3. There is an error on this line can you find it and fix it?     
 for i in range(1, 13):                 #4. Any guess as to what this strange line is supposed to do?
-    print(f"{num} × {i} = num * i}")  #5. There's an error here too. Try to fix it.
+    print(f"{num} * {i} = {num * i}")  #5. There's an error here too. Try to fix it.
